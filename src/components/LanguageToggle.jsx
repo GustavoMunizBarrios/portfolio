0d49b796c41b3bbe2338d9d8d35ceb4ea261ceb9
GustavoMunizBarrios <j.gustavomb1984@hotmail.com>
2024-05-07 20:04:50 -0600

    Adjust language toggle size and positioning; update button styling

--- src/components/LanguageToggle.jsx
+++ src/components/LanguageToggle.jsx
@@ -17,31 +17,35 @@ export default function LanguageToggle() {
 
     return (
         <>
-            <div class="w-[13rem] h-[3rem] ml-[2.5rem] flex text-center content-center items-center rounded-full">
+            <div class="w-[12rem] h-[3rem] ml-[2.5rem] flex text-center content-center items-center rounded-full">
                 <div
                     class="w-full h-[3rem] flex rounded-full items-center content-center"
                 >
                     <div
-                        class="relative w-full rounded-full border h-[3rem] p-1 b-[1px] flex"
+                        class="relative w-full rounded-full h-[3rem] p-1 b-[1px] flex"
                     >
                         <div class="relative w-full h-full flex items-center">
-                            <div class='flex w-[9rem] justify-center'>
+                            <div class='flex w-[8rem] justify-center cursor-pointer pl-2'
+                                onClick={handleLanguageEN}>
 
                                 <button
-                                    class={`mr-3 text-[1rem] z-50 ${!$isLanguageToggle ? 'text-[#7f858f]' : 'null'}`}
-                                    onClick={handleLanguageEN}>EN</button>
+                                    class={`mr-2 text-[1rem] z-50 ${!$isLanguageToggle ? 'text-[#7f858f]' : 'null'}`}
+                                >EN</button>
                                 <img
+                                    class={`${$isLanguageToggle ? 'null' : 'opacity-50'}`}
                                     width="26"
                                     height="26"
                                     src="https://img.icons8.com/color/48/great-britain-circular.png"
                                     alt="great-britain-circular"
                                 />
                             </div>
-                            <div class="flex w-[9rem] justify-center">
+                            <div class="flex w-[8rem] justify-center cursor-pointer pr-2"
+                                onClick={handleLanguageES}>
                                 <button
-                                    class={`mr-3 text-[1rem] z-50 ${$isLanguageToggle ? 'text-[#7f858f]' : 'null'}`}
-                                    onClick={handleLanguageES} >ES</button>
+                                    class={`mr-2 text-[1rem] z-50 ${$isLanguageToggle ? 'text-[#7f858f]' : 'null'}`}
+                                >ES</button>
                                 <img
+                                    class={`${$isLanguageToggle ? 'opacity-50' : 'null'}`}
                                     width="26"
                                     height="26"
                                     src="https://img.icons8.com/color/48/spain2-circular.png"
@@ -51,8 +55,9 @@ export default function LanguageToggle() {
                         </div>
 
                         <span
-                            class="bg-white shadow flex items-center justify-center w-[50%] rounded-full h-[2rem] top-[7px] left-[7px] absolute -z-[12]"
+                            class={`border border-gray-200 shadow flex items-center justify-center w-[50%] rounded-full h-[2.3rem] top-[5px]  absolute -z-[12] ${$isLanguageToggle ? 'left-[7px]' : 'right-[7px]'}`}
                         ></span>
+
                     </div >
                 </div >
             </div >
